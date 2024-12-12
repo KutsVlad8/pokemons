84138ffda7fe98c0a83bde7f910458585ecf3489
Vladyslav Kutsevych <116116220+KutsVlad8@users.noreply.github.com>
2024-12-12 16:54:20 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My React App</h1>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
